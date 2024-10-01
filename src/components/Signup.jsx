f2b9bac54efe504a87d309bfe241deef4047f241
KhDan007 <kh.dan007@gmail.com>
2024-10-01 22:18:27 +0500

    signup page base

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,15 @@
 export const Signup = () => {
     return (
-        <main>
-            <h1>Signup</h1>
+        <main className="bg-blue-500 flex flex-col justify-center items-center h-screen p-5">
+            <div className="container bg-slate-50 flex rounded-2xl shadow-2xl">
+                <div className="w-1/3 bg-orange-300"></div>
+                <div className="w-2/3 p-4 sm:p-6 md:p-8 lg:p-12">
+                    <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold">Welcome to NEWS</h2>
+                    <h3 className="text-slate-500 font-semibold pt-1">Register your account</h3>
+
+
+                </div>
+            </div>
         </main>
     );
 };
