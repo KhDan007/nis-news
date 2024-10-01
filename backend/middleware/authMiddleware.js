dba3c4829296bf2acd285b4f60ec2b5d6fa08a0d
KhDan007 <kh.dan007@gmail.com>
2024-10-01 20:35:52 +0500

    fix jwt issue

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,16 +1,24 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
+    // Get token from header
     const token = req.header("Authorization");
 
+    // Check if no token is present
     if (!token) {
         return res
             .status(401)
             .json({ message: "No token, authorization denied" });
     }
 
+    // Split the token to remove the 'Bearer' prefix
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const bearerToken = token.split(" ")[1]; // Split the 'Bearer' part from the actual token
+
+        // Verify the token
+        const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET);
+
+        // Attach the writer data to the request
         req.writer = decoded.writer;
         next();
     } catch (err) {
