803198f37577f453b569e6e0096c29bf62b1e387
KhDan007 <kh.dan007@gmail.com>
2024-10-07 18:23:56 +0500

    connect api to submit on signup

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -14,7 +14,23 @@ export const MyForm = () => {
         mode: "onBlur",
     });
 
-    const onSubmit = (data) => console.log(data);
+    const onSubmit = (data) => {
+        console.log(data);
+        Axios.post("http://localhost:5000/api/writers/signup", data, {
+            headers: {
+                "Content-Type": "application/json",
+                // Authorization: "Bearer token123",
+            },
+            timeout: 5000,
+            responseType: "json",
+        })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
 
     const validateForm = () => {
         if (errors.name) {
